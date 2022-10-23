6bac517cfd08bb5a8fecbfb38826d67851e6d685
Merna Fekry <mernafekry15@gmail.com>
2022-10-23 15:00:33 +0200

    intial commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+$('.singerRow h4').click(function(){
+    $('.singerRow p').slideUp();
+})
+
+
+
+// $('.row h4:nth-chile()').click(function(){
+//     $('.row p').slideUp();
+// })
+// 
+// $('.row h4').eq(1).eq(2).eq(3).click(function(){
+//     $('.row p').eq(1).eq(2).eq(3).slideUp();
+// })
+
+// $('.row h4:nth-child(1)').click(function(){
+
+//     console.log("Clicked");
+//     $('.row p:nth-child(1)').slideUp();
+// })
